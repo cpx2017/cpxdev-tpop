2a23d05a424c24dbd0d0cc5d866445d544c7083f
Chinnathorn P <chinnathorn.p@gmail.com>
2022-12-05 20:47:49 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'anakotmai',
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <App/>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function

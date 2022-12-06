037ab99dfb9e5c4ef3025bab99d857d2f08bf89c
Chinnathorn P <chinnathorn.p@gmail.com>
2022-12-06 22:22:18 +0700

    beta 3

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
     window.addEventListener('resize', handleWindowResize);
     fetch('https://api.cpxdev.tk/home/status')
       .then((response) => response.text())
-      .then((data) => ;
+      .then((data) => console.log());
     return () => {
       window.removeEventListener('resize', handleWindowResize);
     };

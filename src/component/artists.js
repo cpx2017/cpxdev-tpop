978fe84d9830273508b241ffe96144297349fdcc
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2022-12-07 15:47:00 +0700

    Artist mocking

--- src/component/artists.js
+++ src/component/artists.js
@@ -1,7 +1,53 @@
 import React from 'react'
 import pagedetail from '../menulist/Artists.json'
 import Typography from '@mui/material/Typography';
-import { CardHeader } from '@mui/material';
+import { CardHeader, CardMedia, Card } from '@mui/material';
+
+const a = [
+  {
+    artId: "8c05ea89-d8ac-4b14-b67d-e75f4862ff1b",
+    artImg: "https://i.scdn.co/image/ab6761610000e5eb60bbde73af6f47df1fe8c8d5",
+    artName: {
+      en:"BNK48",
+      th:"บีเอ็นเคโฟตี้เอต"
+    },
+    artType: {
+      en:"group",
+      th:"กลุ่ม"
+    },
+    artlabel: "Independent Artist Management"
+  },
+  {
+    artId: "f2038c70-0ebb-460d-98f0-f84e23399656",
+    artImg: "https://i.scdn.co/image/ab6761610000e5eb3d335d336e2cd328483e18aa",
+    artName: {
+      en:"SCRUBB",
+      th:"สครับ"
+    },
+    artType: {
+      en:"duo",
+      th:"คู่"
+    },
+    artlabel: ""
+  },
+  {
+    artId: "d4a40e1b-7598-4ef8-8fd8-e19fc8e1a0d9",
+    artImg: "https://i.scdn.co/image/ab6761610000e5eb66ce24077a255374fe0ae3aa",
+    artName: {
+      en:"Zommarie",
+      th:"ส้ม มารี"
+    },
+    artType: {
+      en:"single",
+      th:"เดี่ยว"
+    },
+    artlabel: "SpicyDisc"
+  }
+]
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 const Art = ({setLoad, lang}) => {
     const [width, setRealwidth] = React.useState(window.innerWidth);
@@ -29,6 +75,24 @@ const Art = ({setLoad, lang}) => {
     return ( 
         <>
           <CardHeader title={(<h3>{pagedetail[langselect].title}</h3>)} />
+          <div className='container col-12'>
+            <div className='row d-flex justify-content-center'>
+              {a.map((item) => (
+                <Card className='col-md-3 mb-4 text-center pb-4'>
+                  <CardMedia className='pt-3 mb-2' src={item.artImg} component='img' />
+                  <Typography variant='h5'>
+                    {item.artName[langselect]}
+                  </Typography>
+                  <Typography variant='subtitle2'>
+                   {langselect == 'th' && 'ศิลปิน'}{capitalizeFirstLetter(item.artType[langselect])}{langselect == 'en' && ' Artist'}
+                  </Typography>
+                  <Typography variant='body1'>
+                    {item.artlabel != "" ? item.artlabel : "No Music Label (Independent Artist)"}
+                  </Typography>
+                </Card>
+              ))}
+            </div>
+          </div>
         </>
      );
 }
